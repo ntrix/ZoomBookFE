7866fac5d48fce974ed696f2b41d598b3aa97179
ntrix <ntrix@ntien.com>
2020-11-17 11:45:53 +0100

    refector AuthRoute

--- src/containers/AuthRoute.jsx
+++ src/containers/AuthRoute.jsx
@@ -2,26 +2,22 @@ import React, { useState, useEffect } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 function AuthRoute({ render: Component, ...props }) {
-    const [authenticated, setAuthenticated] = useState(null);
+    const [authenticated, setAuthenticated] = useState(0);
+
     useEffect(() => {
-        const user = localStorage.getItem('user');
-        if (user) {
-            setAuthenticated(true);
-        } else {
-            setAuthenticated(false);
-        }
+        setAuthenticated(localStorage.getItem('user')? 1: 2);
     }, []);
-    if (authenticated === null) {
-        return <></>;
-    }
-    return (
+
+    return authenticated? (
         <Route
             {...props}
             render={(props) =>
-                authenticated ? <Component {...props} /> : <Redirect to={'/users/login'} />
+                authenticated === 1
+                    ? <Component {...props} />
+                    : <Redirect to={'/users/login'} />
             }
-        ></Route>
-    );
+        />
+    ): <></>;
 }
 
 export default AuthRoute;
